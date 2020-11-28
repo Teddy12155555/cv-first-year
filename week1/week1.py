5f1d26089260d17adbe5ee96cdae2c80d059e9dc
Taiming <mickey23405383@gmail.com>
2020-11-29 06:20:04 +0800

    done

--- week1/week1.py
+++ week1/week1.py
@@ -1,6 +1,7 @@
 from cv2 import cv2
 from enum import Enum
 from tqdm import tqdm
+from argparse import ArgumentParser
 import face_recognition
 
 class VideoFormat(Enum):
@@ -8,6 +9,7 @@ class VideoFormat(Enum):
     
 class VideoFaceDetection:
     def __init__(self,fileName):
+        self.IFileName = fileName
         self.Capture = cv2.VideoCapture(f"{fileName}")
         self.Width = int(self.Capture.get(cv2.CAP_PROP_FRAME_WIDTH))
         self.Height = int(self.Capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -16,39 +18,77 @@ class VideoFaceDetection:
 
     def setOutputFile(self,format,fileName):
         filepath = "./output/" + fileName
+        self.OFileName = filepath
         self.Format = cv2.VideoWriter_fourcc(*f'{format.value}')
         self.Output = cv2.VideoWriter(filepath, self.Format, self.FPS, (self.Width, self.Height))
 
-    def detect(self):
+    def detect(self,gpu):
+        print("Running ...")
+        print("Press Enter to exit")
         progress = tqdm(total=self.TotalFrames)
-        while True:
-            ret, frame = self.Capture.read()
-            if frame is None:
-                break
+        if gpu:
+            while True:
+                ret, frame = self.Capture.read()
+                if frame is None:
+                    break
 
-            rgb_frame = frame[:, :, ::-1]
-            face_locations = face_recognition.face_locations(rgb_frame,model="cnn")
-            for top, right, bottom, left in face_locations:
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), cv2.LINE_AA)
+                rgb_frame = frame[:, :, ::-1]
+                face_locations = face_recognition.face_locations(rgb_frame,model="cnn")
+                for top, right, bottom, left in face_locations:
+                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), cv2.LINE_AA)
 
-            self.Output.write(frame)
+                self.Output.write(frame)
 
-            cv2.imshow('Frames', frame)
-            progress.update(1)
-            # enter = ascii 13
-            if cv2.waitKey(3) == 13:
-                break
-            
+                cv2.imshow('Frames', frame)
+                progress.update(1)
+                # enter = ascii 13
+                if cv2.waitKey(3) == 13:
+                    break
+        else:
+           while True:
+                ret, frame = self.Capture.read()
+                if frame is None:
+                    break
+
+                rgb_frame = frame[:, :, ::-1]
+                face_locations = face_recognition.face_locations(rgb_frame)
+                for top, right, bottom, left in face_locations:
+                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), cv2.LINE_AA)
+
+                self.Output.write(frame)
+
+                cv2.imshow('Frames', frame)
+                progress.update(1)
+                # enter = ascii 13
+                if cv2.waitKey(3) == 13:
+                    break
+
     def describe(self):
+        print("---------- Info ----------")
+        print(f"Input : {self.IFileName}")
+        print(f"Output : {self.OFileName}")
         print(f"FPS : {self.FPS}")
         print(f"TotalFrames : {self.TotalFrames}")
         print(f"Width : {self.Width}")
         print(f"Height : { self.Height}")
+        print("---------- Start ----------")
         
-        
+
 if __name__ == "__main__":
-    vfd = VideoFaceDetection("input_720p.mp4")
-    vfd.setOutputFile(format=VideoFormat.MPEG_4,fileName="Temp.mp4")
+
+    parser = ArgumentParser()
+    parser.add_argument("-i", help="input video file name",dest="input")
+    parser.add_argument("-o", help="output video file name", dest="output")
+    parser.add_argument("-gpu", help="if use gpu", dest="gpu")
+
+    args = parser.parse_args()
+    flag = False
+    if bool(args.gpu):
+        flag = True
+    
+    vfd = VideoFaceDetection(args.input)
+    vfd.setOutputFile(format=VideoFormat.MPEG_4,fileName=args.output)
     vfd.describe()
-    vfd.detect()
+    print("")
+    vfd.detect(flag)
     
